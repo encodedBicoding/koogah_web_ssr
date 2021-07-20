9bfed96fb67256dbc917c5835ddf8c79b2d54d98
Derinsola16 <aderinsolaodusanya@gmail.com>
2021-07-20 14:50:12 +0100

    added bg image

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,19 +1,35 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const ejs = require("ejs");
 
 const indexRoutes = express.Router();
-const view_path = 'views';
+const view_path = "views";
 
-indexRoutes.get('/', function(req, res, next) {
-  ejs.renderFile('views/index.ejs', {
-    message: 'Hello Worldsss'
-  },
-    {}, function (err, template) {
+indexRoutes.get("/", function (req, res, next) {
+  ejs.renderFile(
+    "views/index.ejs",
+    {
+      message: "Hello Worldsss",
+    },
+    {},
+    function (err, template) {
       if (err) throw err;
       res.end(template);
     }
   );
 });
 
+indexRoutes.get("/faq", function (req, res, next) {
+  ejs.renderFile(
+    "views/faq.ejs",
+    {
+      message: "Faq",
+    },
+    {},
+    function (err, template) {
+      if (err) throw err;
+      res.end(template);
+    }
+  );
+});
 
 module.exports = indexRoutes;
