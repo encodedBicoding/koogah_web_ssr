386301ccf27730474a50daef13823e53e95112cd
encodedBicoding <dominicisioma000@gmail.com>
2021-08-21 20:51:59 +0100

    adds subscription feature

--- public/js/subscribe.js
+++ public/js/subscribe.js
@@ -1,27 +1,63 @@
 function activateSubscribe() {
-  console.log('active');
+  let input_email = '';
   // first get players.
   const all_email_submit_input = document.querySelectorAll('.subscribe_input_email');
   const all_subscribe_submit_btn = document.querySelectorAll('.subscribe_btn');;
   updateSubscribeBtn(all_subscribe_submit_btn, 'Subscribe');
-}
+  activateClick(all_subscribe_submit_btn);
 
-function updateSubscribeBtn(elementArray, text) {
-  Array.from(elementArray).forEach((element => element.innerHTML = text));
-}
-
-function subscribeUser(e) {
-  try {
-    e.preventDefault();
-    console.log('working');
-  } catch (err) {
-    console.log(err);
-  }
-}
- 
-function activateClick(elementArray) {
-  Array.from(elementArray).forEach(element => {
-    element.addEventListener('click', subscribeUser);
+  Array.from(all_email_submit_input).forEach((ele) => {
+    ele.addEventListener('keyup', (e) => {
+      input_email = e.currentTarget.value;
+    });
   });
+
+  function updateSubscribeBtn(elementArray, text) {
+    Array.from(elementArray).forEach((element => element.innerHTML = text));
+  }
+  function resetSubscriptionInput() {
+    Array.from(all_email_submit_input).forEach((ele) => {
+      ele.value = '';
+    });
+  };
+
+  async function subscribeUser(e) {
+    try {
+      e.preventDefault();
+      if (input_email.length <= 0) {
+        alert('please input email address');
+        return false;
+      } else {
+        updateSubscribeBtn(all_subscribe_submit_btn, 'Loading...');
+        await fetch(
+          `/subscribe/email-list/${input_email}`,
+          {
+            method: 'GET',
+            credentials: 'same-origin',
+            mode: 'cors',
+          }
+        ).then(resp => resp.json())
+          .then(result => {
+            updateSubscribeBtn(all_subscribe_submit_btn, 'Subscribe');
+            if (result.status === 200) {
+              resetSubscriptionInput();
+              alert(result.message);
+            } else {
+              alert(result.error);
+            }
+          }).catch(err => {
+            console.log(err);
+          })
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+   
+  function activateClick(elementArray) {
+    Array.from(elementArray).forEach(element => {
+      element.addEventListener('click', subscribeUser);
+    });
+  }
 }
 activateSubscribe();
