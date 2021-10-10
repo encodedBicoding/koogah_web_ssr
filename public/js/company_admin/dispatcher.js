a9780394c347e406ab14d8174a0695945f122659
encodedBicoding <dominicisioma000@gmail.com>
2021-10-10 18:36:28 +0100

    replace ws for other screens

--- public/js/company_admin/dispatcher.js
+++ public/js/company_admin/dispatcher.js
@@ -160,13 +160,14 @@ const vm = new Vue({
       }
     },
   },
-  created() {
+  async created() {
     // connect to websocket.
     // listen for notification
     const self = this;
-    let connectionString = 'wss://koogah-api-staging.herokuapp.com/data_seeking'
-    let mainConnectionString = 'wss://core.koogahapis.com/data_seeking';
-    let localConnectionString = 'ws://localhost:4000/data_seeking';
+    const ws_string_response = await fetch(`${this.host}/api/company/admin/ws/connect`).then((resp => resp.json())).then((res) => res);
+    let connectionString = `wss://koogah-api-staging.herokuapp.com${ws_string_response.connection_url}`;
+    let mainConnectionString = `wss://core.koogahapis.com${ws_string_response.connection_url}`;
+    let localConnectionString = `ws://localhost:4000${ws_string_response.connection_url}`;
     const webSocket = new WebSocket(mainConnectionString);
     webSocket.onopen = function () {
       self.socket = webSocket;
